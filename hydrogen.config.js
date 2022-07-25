bd1e1c29ef52ecf935a633952554cb40c349b4d2
bendrobinson1998 <benjidrobinson@gmail.com>
2022-07-25 22:54:49 +0100

    initial commit

---
+++ hydrogen.config.js
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'felia-reusable-wear.myshopify.com',
+    storefrontToken: 'f76402403066b3c59b71cb072c3a9f7c',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
