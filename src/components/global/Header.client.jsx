81a521ef8d67616b332dc8b09ce8e377c4c00582
bendrobinson1998 <benjidrobinson@gmail.com>
2022-07-26 22:12:58 +0100

    master

--- src/components/global/Header.client.jsx
+++ src/components/global/Header.client.jsx
@@ -66,7 +66,7 @@ function MobileHeader({countryCode, title, isHome, openCart, openMenu}) {
     button: 'relative flex items-center justify-center w-8 h-8',
     container: `${
       isHome
-        ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
+        ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary'
         : 'bg-contrast/80 text-primary'
     } ${
       y > 50 && !isHome ? 'shadow-lightHeader ' : ''
